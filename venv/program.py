e66ed6c59b16b66b18326317faf841f47f34d7c8
Sofia <culiksofi@yandex.ru>
2020-12-12 18:46:25 +0300

    Вторая версия

--- venv/program.py
+++ venv/program.py
@@ -29,13 +29,8 @@ class Example(QWidget):
         self.repaint()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 0, 0))
-        qp.drawRect(30, 30, 120, 30)
-        qp.setBrush(QColor(0, 255, 0))
-        qp.drawRect(30, 60, 120, 30)
-        qp.setBrush(QColor(0, 0, 255))
-        qp.drawRect(30, 90, 120, 30)
-
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(30, 30, 120, 120)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
